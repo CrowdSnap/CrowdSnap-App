1397c06efef91dc6c9219b8fb657889dad619aa3
Alejandro Marcos <alex03marcos@gmail.com>
2024-04-03 17:00:55 +0200

    Añadir botón de inicio de sesión de Google y actualizar assets en pubspec.yaml

--- lib/presentation/screens/signin/sign_in_screen.dart
+++ lib/presentation/screens/signin/sign_in_screen.dart
@@ -1,14 +1,13 @@
 import 'package:crowd_snap/presentation/providers/auth/google/auth_google_repo_provider.dart';
+import 'package:crowd_snap/presentation/widgets/GoogleSignInButton.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
-
 
 class SignInScreen extends ConsumerWidget {
   const SignInScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
     final authGoogle = ref.watch(authGoogleProvider);
 
     return Scaffold(
@@ -16,15 +15,15 @@ class SignInScreen extends ConsumerWidget {
         title: const Text('Sign In'),
       ),
       body: Center(
-        child: ElevatedButton(
-          child: authGoogle.when(
-            data: (_) => const Text('Sign in with Google'),
-            loading: () => const CircularProgressIndicator(),
-            error: (error, _) => Text('Error: $error'),
+        child: authGoogle.when(
+          data: (_) => GoogleSignInButton(
+            onPressed: () => ref.read(authGoogleProvider),
           ),
-          onPressed: () {
-            ref.read(authGoogleProvider);
-          }
+          loading: () => GoogleSignInButton(
+            isLoading: true,
+            onPressed: () {},
+          ),
+          error: (error, _) => Text('Error: $error'),
         ),
       ),
     );
