50675aedb9acc695a3f474735add566f26c5a64a
Alejandro Marcos <alex03marcos@gmail.com>
2024-05-04 17:02:27 +0200

    feat: Update dark mode logic to use device brightness

--- lib/app/theme/notifier/theme_notifier.dart
+++ lib/app/theme/notifier/theme_notifier.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'theme_notifier.g.dart';
@@ -5,7 +6,12 @@ part 'theme_notifier.g.dart';
 @riverpod
 class DarkMode extends _$DarkMode {
   @override
-  bool build() => false;
+  bool build() {
+    // Get the current brightness mode of the device
+    final brightness = WidgetsBinding.instance.platformDispatcher.platformBrightness;
+    // Return true if dark mode, false if light mode
+    return brightness == Brightness.dark;
+  }
 
   void toggleDarkMode() {
     state = !state;
