d99b52cc481be7405a2522d3b1fa747db92bb236
Alejandro Marcos <alex03marcos@gmail.com>
2024-05-08 10:19:08 +0200

    Appbar dinamica dependiendo del desplazamiento, refres de la pantalla al deslizar hacia abajo, y se cargan mas posts al seguir deslizando hacia arriba

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,36 +1,106 @@
-import 'package:crowd_snap/features/home/presentation/provider/post_provider.dart';
+import 'package:crowd_snap/features/home/presentation/provider/post_state_notifier.dart';
 import 'package:crowd_snap/features/imgs/presentation/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Clase `HomeView` que extiende `ConsumerWidget` para utilizar datos de proveedores.
-class HomeView extends ConsumerWidget {
-  
-  // Constructor simple sin parámetros.
+class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
 
-  // Método `build` que define la interfaz de usuario de la pantalla de inicio.
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  final ScrollController _scrollController = ScrollController();
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (!_isLoading &&
+        _scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent) {
+      _loadMorePosts();
+    }
+  }
+
+  Future<void> _loadMorePosts() async {
+    setState(() {
+      _isLoading = true;
+    });
+    await ref.read(postListProvider.notifier).loadMorePosts();
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _scrollToTop() {
+    _scrollController.animateTo(
+      0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final postListAsyncValue = ref.watch(postListProvider);
-    
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'), // Título de la aplicación.
-      ),
-      body: postListAsyncValue.when(
-        data: (postList) {
-          return ListView.builder(
-            itemCount: postList.length,
-            itemBuilder: (context, index) {
-              final post = postList[index];
-              return PostCard(post: post);
-            },
-          );
-        },
-        loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stackTrace) => Center(child: Text('Error: $error')),
+      body: RefreshIndicator(
+        onRefresh: () => ref.read(postListProvider.notifier).refreshPosts(),
+        child: postListAsyncValue.when(
+          data: (postList) {
+            return CustomScrollView(
+              controller: _scrollController,
+              slivers: [
+                SliverAppBar(
+                  title: GestureDetector(
+                    onTap: _scrollToTop,
+                    child: const Text('Home'),
+                  ),
+                  pinned: false,
+                  floating: true,
+                  snap: true,
+                ),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index == postList.length) {
+                        return _buildLoadingIndicator();
+                      }
+                      final post = postList[index];
+                      return PostCard(post: post);
+                    },
+                    childCount: postList.length + 1,
+                  ),
+                ),
+              ],
+            );
+          },
+          loading: () => const Center(child: CircularProgressIndicator()),
+          error: (error, stackTrace) => Center(child: Text('Error: $error')),
+        ),
       ),
     );
+  }
+
+  Widget _buildLoadingIndicator() {
+    return _isLoading
+        ? const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Center(child: CircularProgressIndicator()),
+          )
+        : const SizedBox.shrink();
   }
 }
