29f7c76bd166b9821c35e418ba09585d3bd3dd1b
Alejandro Marcos <alex03marcos@gmail.com>
2024-05-28 19:46:34 +0200

    feat: Add lazy loading for images and loading animations to home view

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -4,6 +4,7 @@ import 'package:crowd_snap/features/home/presentation/widgets/filter_button.dart
 import 'package:crowd_snap/features/imgs/presentation/widgets/post_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
@@ -12,19 +13,23 @@ class HomeView extends ConsumerStatefulWidget {
   ConsumerState<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends ConsumerState<HomeView> {
+class _HomeViewState extends ConsumerState<HomeView>
+    with TickerProviderStateMixin {
   final ScrollController _scrollController = ScrollController();
   bool _isLoading = false;
+  late final AnimationController _animationController;
 
   @override
   void initState() {
     super.initState();
     _scrollController.addListener(_onScroll);
+    _animationController = AnimationController(vsync: this);
   }
 
   @override
   void dispose() {
     _scrollController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -65,7 +70,9 @@ class _HomeViewState extends ConsumerState<HomeView> {
         child: postListAsyncValue.when(
           data: (postList) {
             return CustomScrollView(
-              physics: blockScroll ? const NeverScrollableScrollPhysics() : const ClampingScrollPhysics(),
+              physics: blockScroll
+                  ? const NeverScrollableScrollPhysics()
+                  : const ClampingScrollPhysics(),
               controller: _scrollController,
               slivers: [
                 SliverAppBar(
@@ -101,7 +108,35 @@ class _HomeViewState extends ConsumerState<HomeView> {
               ],
             );
           },
-          loading: () => const Center(child: CircularProgressIndicator()),
+          loading: () => Scaffold(
+            appBar: AppBar(
+              title: Image.asset(
+                'assets/icons/crowd_snap_logo.png',
+                height: 165,
+                width: 85,
+                fit: BoxFit.cover,
+              ),
+              actions: const [
+                FilterButton(),
+              ],
+            ),
+            body: Center(
+              child: Lottie.asset(
+                'assets/Principal-Disco-Ball.json',
+                controller: _animationController,
+                onLoaded: (composition) {
+                  _animationController
+                    ..duration = composition.duration *
+                        (2 / 3) // Ajustar la duración para 1.5x velocidad
+                    ..forward(
+                        from: 0.05); // Saltar los primeros 20% de la animación
+                },
+                height: 400,
+                width: 400,
+                repeat: true,
+              ),
+            ),
+          ),
           error: (error, stackTrace) => Center(child: Text('Error: $error')),
         ),
       ),
@@ -110,9 +145,17 @@ class _HomeViewState extends ConsumerState<HomeView> {
 
   Widget _buildLoadingIndicator() {
     return _isLoading
-        ? const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Center(child: CircularProgressIndicator()),
+        ? Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Lottie.asset(
+              'assets/Secondary-Disco-Ball.json',
+              onLoaded: (composition) {
+                _animationController.duration = composition.duration *
+                    (4 / 5); // Ajustar la duración para 1.5x velocidad
+              },
+              height: 75,
+              width: 75,
+            ),
           )
         : const SizedBox.shrink();
   }
