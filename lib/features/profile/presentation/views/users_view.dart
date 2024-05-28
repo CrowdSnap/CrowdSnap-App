050bd0811fc30aab4fb9387c26180e66d3f9f20e
Alejandro Marcos <alex03marcos@gmail.com>
2024-05-28 12:44:38 +0200

    Conexion entre usuarios y optimizaciones de la pantalla users

--- lib/features/profile/presentation/views/users_view.dart
+++ lib/features/profile/presentation/views/users_view.dart
@@ -2,20 +2,16 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crowd_snap/core/data/models/post_model.dart';
 import 'package:crowd_snap/core/data/models/user_model.dart';
 import 'package:crowd_snap/core/domain/use_cases/shared_preferences/get_user_local_use_case.dart';
+import 'package:crowd_snap/features/profile/data/repositories_impl/user_posts_repository_impl.dart';
+import 'package:crowd_snap/features/profile/data/repositories_impl/users_repository_impl.dart';
 import 'package:crowd_snap/features/profile/domain/use_cases/add_connection_use_case.dart';
-import 'package:crowd_snap/features/profile/domain/use_cases/get_user_posts_use_case.dart';
-import 'package:crowd_snap/features/profile/domain/use_cases/get_user_use_case.dart';
 import 'package:crowd_snap/features/profile/domain/use_cases/remove_connection_use_case.dart';
-import 'package:crowd_snap/features/profile/presentation/notifier/connection_status_provider.dart';
-import 'package:crowd_snap/features/profile/presentation/notifier/connections_counter.dart';
-import 'package:crowd_snap/features/profile/presentation/notifier/is_connected_provider.dart';
-import 'package:crowd_snap/features/profile/presentation/notifier/users_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:skeletonizer/skeletonizer.dart';
 
-class UsersView extends ConsumerWidget {
+class UsersView extends ConsumerStatefulWidget {
   final String userId;
   final String username;
   final String avatarUrl;
@@ -30,91 +26,132 @@ class UsersView extends ConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final userProfileAsyncValue = ref.watch(userProviderProvider(userId));
-    final userPostsAsyncValue = ref.watch(userPostsProviderProvider(userId));
-    final connectionStatusAsyncValue =
-        ref.watch(connectionStatusProviderProvider(userId));
+  _UsersViewState createState() => _UsersViewState();
+}
 
-    return userProfileAsyncValue.when(
-      data: (user) {
-        // Inicializar el contador de conexiones con el valor del usuario
-        ref.read(connectionsCounterProvider(user.connectionsCount).notifier);
+class _UsersViewState extends ConsumerState<UsersView> {
+  late PageController pageController;
+  late Future<void> _initializationFuture;
+  late UserModel localUser;
+  late UserModel user;
+  late List<PostModel> userPosts;
+  int index = 0;
+  bool isConnected = false;
+  int connectionsCount = 0;
 
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(
-                '@$username'), // Mostrar el nombre del usuario en el AppBar
-          ),
-          body: _buildUserProfile(context, user, userPostsAsyncValue,
-              connectionStatusAsyncValue, ref),
-        );
-      },
-      loading: () => Scaffold(
-        appBar: AppBar(
-          title:
-              Text('@$username'), // Mostrar el nombre del usuario en el AppBar
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Center(
-                child: SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: ClipOval(
-                    child: BlurHash(
-                      hash: blurHashImage,
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+    _initializationFuture = _initializeData();
+  }
+
+  Future<void> _initializeData() async {
+    final localUser = await ref.read(getUserLocalUseCaseProvider).execute();
+    final user = await ref.read(usersRepositoryProvider).getUser(widget.userId);
+    final userPosts = await ref.read(userPostsRepositoryProvider).getUserPosts(widget.userId);
+    final isConnected = await ref.read(usersRepositoryProvider).checkConnection(localUser.userId, widget.userId);
+    print('isConnected: $isConnected');
+    print('user: $user');
+    print('userPosts: $userPosts');
+    print('localUser: $localUser');
+
+    if (mounted) {
+      setState(() {
+        this.localUser = localUser;
+        this.user = user;
+        this.userPosts = userPosts;
+        this.isConnected = isConnected;
+        this.connectionsCount = user.connectionsCount;
+      });
+    }
+  }
+
+  void _toggleConnection() {
+    if (isConnected) {
+      ref.read(removeConnectionUseCaseProvider).execute(localUser.userId, widget.userId);
+      setState(() {
+        connectionsCount--;
+        isConnected = false;
+      });
+    } else {
+      ref.read(addConnectionUseCaseProvider).execute(localUser.userId, widget.userId);
+      setState(() {
+        connectionsCount++;
+        isConnected = true;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initializationFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('@${widget.username}'), // Mostrar el nombre del usuario en el AppBar
+            ),
+            body: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: SizedBox(
+                      width: 150,
+                      height: 150,
+                      child: ClipOval(
+                        child: BlurHash(
+                          hash: widget.blurHashImage,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(height: 16),
-            Skeletonizer(
-              enabled: true,
-              child: Container(
-                width: 300,
-                height: 30,
-                decoration: BoxDecoration(
-                  color: Colors.grey[400],
-                  borderRadius: BorderRadius.circular(20),
+                const SizedBox(height: 16),
+                Skeletonizer(
+                  enabled: true,
+                  child: Container(
+                    width: 300,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[400],
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-      error: (error, stack) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Error'), // Título en caso de error
-        ),
-        body: const Center(
-            child: Text('Usuario no encontrado probablemente se eliminó')),
-      ),
+          );
+        } else if (snapshot.hasError) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('@${widget.username}'), // Mostrar el nombre del usuario en el AppBar
+            ),
+            body: Center(
+              child: Text('Error: ${snapshot.error}'),
+            ),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('@${widget.username}'), // Mostrar el nombre del usuario en el AppBar
+            ),
+            body: _buildUserProfile(context),
+          );
+        }
+      },
     );
   }
 
-  Future<UserModel> getLocalUser(WidgetRef ref) async {
-    final getUserUseCase = ref.read(getUserLocalUseCaseProvider);
-    return await getUserUseCase.execute();
-  }
-
-  Widget _buildUserProfile(
-      BuildContext context,
-      UserModel user,
-      AsyncValue<List<PostModel>> userPostsAsyncValue,
-      AsyncValue<bool> connectionStatusAsyncValue,
-      WidgetRef ref) {
-    final userValues = ref.watch(usersNotifierProvider);
-    final usersNotifier = ref.read(usersNotifierProvider.notifier);
-    final pageController = PageController();
-    final counterState =
-        ref.watch(connectionsCounterProvider(user.connectionsCount));
-    final isconnectedState =
-        ref.watch(connectionStatusProviderProvider(user.userId));
-
+  Widget _buildUserProfile(BuildContext context) {
     return Column(
       children: [
         Padding(
@@ -158,77 +195,18 @@ class UsersView extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              'Conexiones: ${counterState.count}',
+              'Conexiones: $connectionsCount',
               style: TextStyle(
                 fontSize: 20,
                 color: Colors.grey[700],
               ),
             ),
-            const SizedBox(height: 8),
-            FutureBuilder<UserModel>(
-              future: getLocalUser(ref),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Skeletonizer(
-                    child: Container(
-                      width: 200,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[400],
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else if (snapshot.hasData) {
-                  final localUser = snapshot.data!;
-                  if (localUser.userId == user.userId) {
-                    return Container(); // No mostrar el botón si es el mismo usuario
-                  } else {
-                    return connectionStatusAsyncValue.when(
-                      data: (isFollowing) => ElevatedButton(
-                        onPressed: () {
-                          if (isFollowing) {
-                            ref
-                                .read(removeConnectionUseCaseProvider)
-                                .execute(localUser.userId, user.userId);
-                            ref
-                                .read(connectionsCounterProvider(
-                                        user.connectionsCount)
-                                    .notifier)
-                                .decrement();
-                            ref
-                                .read(isConnectedProvider(isFollowing).notifier)
-                                .setDisconnected();
-                          } else {
-                            ref
-                                .read(addConnectionUseCaseProvider)
-                                .execute(localUser.userId, user.userId);
-                            ref
-                                .read(connectionsCounterProvider(
-                                        user.connectionsCount)
-                                    .notifier)
-                                .increment();
-                            ref
-                                .read(isConnectedProvider(isFollowing).notifier)
-                                .setDisconnected();
-                          }
-                        },
-                        child: Text(isFollowing
-                            ? 'Desconectar'
-                            : 'Conectar con ${user.name.split(' ')[0]}'),
-                      ),
-                      loading: () => Container(),
-                      error: (error, stack) => Text('Error: $error'),
-                    );
-                  }
-                } else {
-                  return Container();
-                }
-              },
-            ),
             const SizedBox(height: 16),
+            if (localUser.userId != user.userId)
+            ElevatedButton(
+              onPressed: _toggleConnection,
+              child: Text(isConnected ? 'Desconectar' : 'Conectar'),
+            ),
           ],
         ),
         Row(
@@ -236,132 +214,74 @@ class UsersView extends ConsumerWidget {
           children: [
             IconButton(
               icon: const Icon(Icons.grid_on),
-              color: userValues.index == 0
+              color: index == 0
                   ? Theme.of(context).colorScheme.primary
                   : Colors.grey,
-              onPressed: () =>
-                  _onGridSelected(0, usersNotifier, pageController),
+              onPressed: () => _onGridSelected(0),
             ),
             IconButton(
               icon: const Icon(Icons.person),
-              color: userValues.index == 1
+              color: index == 1
                   ? Theme.of(context).colorScheme.primary
                   : Colors.grey,
-              onPressed: () =>
-                  _onGridSelected(1, usersNotifier, pageController),
+              onPressed: () => _onGridSelected(1),
             ),
           ],
         ),
         const SizedBox(height: 16),
         Expanded(
-          child: AnimatedSwitcher(
-            duration: const Duration(milliseconds: 500),
-            child: userPostsAsyncValue.when(
-              data: (posts) => PageView(
-                key: const ValueKey('posts'),
-                controller: pageController,
-                onPageChanged: (index) {
-                  usersNotifier.updateIndex(index);
+          child: PageView(
+            controller: pageController,
+            onPageChanged: (index) {
+              setState(() {
+                this.index = index;
+              });
+            },
+            children: [
+              GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                ),
+                itemCount: userPosts.length,
+                itemBuilder: (context, index) {
+                  final post = userPosts[index];
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image(
+                      image: CachedNetworkImageProvider(post.imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                  );
                 },
-                children: [
-                  GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 5,
-                    ),
-                    itemCount: posts.length,
-                    itemBuilder: (context, index) {
-                      final post = posts[index];
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Image(
-                          image: CachedNetworkImageProvider(post.imageUrl),
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  ),
-                  ListView.builder(
-                    itemCount: posts.length,
-                    itemBuilder: (context, index) {
-                      final post = posts[index];
-                      return ListTile(
-                        leading: CircleAvatar(
-                          backgroundImage:
-                              CachedNetworkImageProvider(post.imageUrl),
-                        ),
-                        title: Text(post.userName),
-                        subtitle: Text(post.description ?? ''),
-                      );
-                    },
-                  ),
-                ],
               ),
-              loading: () => PageView(
-                key: const ValueKey('loading'),
-                controller: pageController,
-                children: [
-                  GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 5,
+              ListView.builder(
+                itemCount: userPosts.length,
+                itemBuilder: (context, index) {
+                  final post = userPosts[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: CachedNetworkImageProvider(post.imageUrl),
                     ),
-                    itemCount: 9, // Número de placeholders que deseas mostrar
-                    itemBuilder: (context, index) {
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Skeletonizer(
-                          enabled: true,
-                          effect:
-                              const ShimmerEffect(), // Añadir efecto shimmer
-                          child: Container(
-                            color: Colors.grey[800],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      crossAxisSpacing: 5,
-                      mainAxisSpacing: 5,
-                    ),
-                    itemCount: 9, // Número de placeholders que deseas mostrar
-                    itemBuilder: (context, index) {
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(8),
-                        child: Skeletonizer(
-                          enabled: true,
-                          effect:
-                              const ShimmerEffect(), // Añadir efecto shimmer
-                          child: Container(
-                            color: Colors.grey[800],
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ],
+                    title: Text(post.userName),
+                    subtitle: Text(post.description ?? ''),
+                  );
+                },
               ),
-              error: (error, stack) => Center(child: Text('Error: $error')),
-            ),
+            ],
           ),
         ),
       ],
     );
   }
 
-  void _onGridSelected(
-      int index, UsersNotifier usersNotifier, PageController pageController) {
-    usersNotifier.updateIndex(index);
+  void _onGridSelected(int index) {
+    setState(() {
+      this.index = index;
+    });
     pageController.animateToPage(
-      index,
+      this.index,
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
     );
